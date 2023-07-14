fb1760bff1d04a5cad6d233d7cf9fac7fb830960
AmirMohammad Bagheri <amirmbagherii17@gmail.com>
2023-07-15 01:32:17 +0330

    * Add main project

---
+++ PagingCore/PagingCore/Extensions/Paging.cs
@@ -0,0 +1,50 @@
+﻿using PagingCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PagingCore.Extensions
+{
+    public static class Paging
+    {
+        public static PagedList<TSource> ToPagedList<TSource>(this List<TSource> source, 
+            int currentPage = 1,
+            int itemsPerPage = 15)
+        {
+            int skip = (currentPage - 1) * itemsPerPage;
+            PagedList<TSource> result = new PagedList<TSource>();
+            result.Items = source.Skip(skip).Take(itemsPerPage).ToList();
+            result.Total = source.LongCount();
+            result.CurrentPage = currentPage;
+            result.TotalPages = (int)Math.Ceiling((double)result.Total / (double)itemsPerPage);
+            result.ItemsPerPage = itemsPerPage;
+
+            return result;
+        }
+
+        public static PagedList<TSource> NextPage<TSource>(this PagedList<TSource> source,
+            int itemsPerPage = 15)
+        {
+            source.CurrentPage += 1;
+            int skip = (source.CurrentPage - 1) * itemsPerPage;
+
+            source.Items = source.Items.Skip(skip).Take(itemsPerPage).ToList();
+            source.ItemsPerPage = itemsPerPage;
+
+            return source;
+        }
+
+        public static PagedList<TSource> PreviousPage<TSource>(this PagedList<TSource> source,
+            int itemsPerPage = 15)
+        {
+            source.CurrentPage -= 1;
+            int skip = (source.CurrentPage - 1) * itemsPerPage;
+
+            source.Items = source.Items.Skip(skip).Take(itemsPerPage).ToList();
+            source.ItemsPerPage = itemsPerPage;
+
+            return source;
+        }
+    }
+}
---
+++ PagingCore/PagingCore/Models/PagedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagingCore.Models
+{
+    public class PagedList<TSource>
+    {
+        public List<TSource> Items { get; set; } = new List<TSource>();
+        public long Total { get; internal set; } = 0;
+        public int CurrentPage { get; internal set; } = 1;
+        public int TotalPages { get; set; } = 0;
+        public int ItemsPerPage { get; set; } = 15;
+    }
+}
